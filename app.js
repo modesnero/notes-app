f7cf33f12fc5f4965eabf8fecfffef0460609416
modesnero <modesnero@gmail.com>
2020-03-25 14:33:05 +0300

    add production route on index.html

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const PORT = config.get('port') || 5000
 const MONGO_URL = config.get('mongoUrl')
@@ -14,6 +15,14 @@ const app = express()
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/note', require('./routes/notes.routes'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 async function start () {
   try {
