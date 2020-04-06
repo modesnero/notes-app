cae3d36b1dc4602c59e1aa7d1793c4b2661b7d5f
modesnero <modesnero@gmail.com>
2020-04-06 06:42:06 +0300

    preparation for deploy

--- app.js
+++ app.js
@@ -4,15 +4,6 @@ const mongoose = require('mongoose')
 const path = require('path')
 
 const PORT = config.get('port') || 5000
-const MONGO_URL = config.get('mongoUrl')
-const MONGO_OPTIONS = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-}
-
-const PATH_CLIENT_STATIC = path.join(__dirname, 'client', 'build')
-const PATH_INDEX_PAGE = path.resolve(__dirname, 'client', 'build', 'index.html')
 
 const app = express()
 
@@ -22,13 +13,19 @@ app.use('/api/notes', require('./routes/notes.routes'))
 
 // Send static files on Prod
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(PATH_INDEX_PAGE))
-  app.get('*', (_req, res) => res.sendFile(PATH_CLIENT_STATIC))
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+  app.get('*', (_req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
 }
 
 async function start () {
   try {
-    await mongoose.connect(MONGO_URL, MONGO_OPTIONS)
+    await mongoose.connect(config.get('mongoUrl'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
     app.listen(PORT, () => console.log(`App has been started on port: ${PORT}`))
   } catch (err) {
     console.error('Server Error', err.message)
